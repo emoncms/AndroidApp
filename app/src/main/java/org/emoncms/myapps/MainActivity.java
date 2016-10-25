157c1ab3e7484e3ff9061edd766165fca9048c40
JumpMaster <kevin@cooper.uk.com>
2016-10-25 20:45:38 +0100

    Applied German Translation. Renamed BarcodeScanner folder to barcodescanner

--- app/src/main/java/org/emoncms/myapps/MainActivity.java
+++ app/src/main/java/org/emoncms/myapps/MainActivity.java
@@ -10,7 +10,6 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.GestureDetector;
